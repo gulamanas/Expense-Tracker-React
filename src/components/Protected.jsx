45935574f9f4fda4de359239abeef8c6fca60901
Gulam Anas <g.anas786@gmail.com>
2024-02-06 00:19:05 +0530

    fix

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -3,7 +3,9 @@ import { Navigate, Outlet } from 'react-router-dom';
 const Protected = () => {
   const token = localStorage.getItem('token');
   const user = localStorage.getItem('user');
-  const userToken = user ?? JSON.parse(user).stsTokenManager.accessToken;
+  console.log({ user });
+  let userToken =
+    user != null ? JSON.parse(user).stsTokenManager.accessToken : '';
 
   return token === userToken ? <Outlet /> : <Navigate to='/login' />;
 };
